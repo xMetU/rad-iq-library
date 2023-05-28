1c7da2981a6195970d700bd8a0407d127c216dae
Matthew Owen <matthew.owen@y7mail.com>
2023-05-28 16:21:23 +0930

    update image test data

--- administrator/components/com_myimageviewer/sql/install.mysql.utf8.sql
+++ administrator/components/com_myimageviewer/sql/install.mysql.utf8.sql
@@ -19,6 +19,8 @@ CREATE TABLE IF NOT EXISTS `#__myImageViewer_image` (
 	UNIQUE KEY `unique_imageName_categoryId` (`imageName`, `categoryId`)
 ) ENGINE = InnoDB;
 
+
+
 INSERT INTO `#__myImageViewer_imageCategory` (`categoryName`) VALUES
 	('Hip'),
 	('Acetabulum'),
@@ -48,3 +50,11 @@ INSERT INTO `#__myImageViewer_imageCategory` (`categoryName`) VALUES
 	('Norgaard\'s'),
 	('Wrist'),
 	('Scaphoid');
+
+INSERT INTO `#__myimageviewer_image` (`imageName`, `categoryId`, `imageDescription`, `imageUrl`, `isHidden`) VALUES
+('An x-ray of someones wrists, they look to be pretty healthy.', 26, CONCAT(REPEAT('This is a lengthy description that will test overflow handling. ', 180)), 'media/com_myImageViewer/images/Norgaard\'s/An x-ray of someones wrists, they look to be pretty healthy..jpeg', 0),
+('Ankle x-ray', 4, '', 'media/com_myImageViewer/images/Ankle/Ankle x-ray.png', 0),
+('Chest x-ray 1', 11, 'This is an x-ray of a chest.', 'media/com_myImageViewer/images/Chest/Chest x-ray 1.jpg', 0),
+('Chest x-ray 2', 11, 'This is also an x-ray of a chest.', 'media/com_myImageViewer/images/Chest/Chest x-ray 2.png', 0),
+('Arm (not broken)', 17, 'This is an x-ray image of an arm that is not broken. If your arm does not look like this, please consult a doctor.', 'media/com_myImageViewer/images/Forearm/Arm (not broken).png', 0),
+('Example shoulder image', 19, 'This is a shoulder. The shoulder is important because we would not be able to carry backpacks without it.', 'media/com_myImageViewer/images/Shoulder/Example shoulder image.png', 0);
